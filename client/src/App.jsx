914786c99f7ca41645d794364b4cc9d9eda60ff5
sibi <ssibi769@gmail.com>
2024-12-26 21:25:13 +0530

    install

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-black text-white p-4">
+      <h1 className="text-3xl text-purple-600 shadow-lg p-4">App</h1>
+    </div>
+  );
+}
+
+export default App;
