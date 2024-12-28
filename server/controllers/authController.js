500b51493710a6e5606d649d2e07f743cdea18a9
sibi <ssibi769@gmail.com>
2024-12-28 14:39:56 +0530

    Add a middleWare

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,13 +1,16 @@
 import User from "../models/user.models.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
 
   if (!username || !email || !password) {
-    return res.status(400).json({ Message: "All Fields Are Required!" });
+    next(errorHandler(400, "All Fields Are Required!"));
+    // return res.status(400).json({ Message: "All Fields Are Required!" });
   } else if (username == " " || email == " " || password == " ") {
-    return res.status(400).json({ Message: "Enter the Valid Info!" });
+    next(errorHandler(400, "Enter the Valid Info!"));
+    // return res.status(400).json({ Message: "Enter the Valid Info!" });
   } else {
     const hashedPassword = bcryptjs.hashSync(password, 10);
     const newUser = new User({
@@ -19,7 +22,8 @@ export const signup = async (req, res) => {
       await newUser.save();
       res.status(200).json({ message: "Signed-Up Successfully!" });
     } catch (error) {
-      res.status(500).json({ Error: error.message });
+      next(error);
+      // res.status(500).json({ Error: error.message });
     }
   }
   //if the value and key are same user the single word *
